1a2e4c72de34bb128934ebcbcbb34960ced260ae
czell <czell@UCL08248.knapp.at>
2016-05-21 14:12:21 +0200

    commit

---
+++ ALD_UE/src/linkedlist/LinkedList.java
@@ -0,0 +1,7 @@
+package linkedlist;
+
+public class LinkedList {
+
+	private Node first;
+	
+}
---
+++ ALD_UE/src/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+public class Node {
+
+	private Integer Value;
+	private Node next;
+	
+	
+	
+}
